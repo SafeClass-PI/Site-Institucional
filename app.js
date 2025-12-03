cb70b21fe979d0e04e521fa79d0321295d98e618
ryanservilha <ryan.servilha@sptech.school>
2025-12-03 15:43:25 -0300

    Atualizando linkagem da página de desempenho semestral

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const salaRouter = require('./public/Dashboard/DASHBOARD_CPU/srcCPU/routesCPU/bu
 // Isso define que o endpoint completo será: /salas/listar
 var ryanRouter = require("./public/Dashboard/DASHBOARD_REDE/srcRyan/routesRyan/ryan");
 var biaRouter = require("./public/Dashboard/DASHBOARD_ACESSOS/srcBeatriz/routesBeatriz/bia");
-var matheusRouter = require("./public/Dashboard/DASHBOARD_MATHEUS/srcMatheus/routesMatheus/matheus");
+var matheusRouter = require("./public/Dashboard/DASHBOARD_SEMESTRE/srcMatheus/routesMatheus/matheus");
 var joaoRouter = require("./public/Dashboard/DASHBOARD_ALERTAS/srcJoao/routesJoao/rotasJoao");
 
 
